36276973d558338f41cdcb3f0599e5228a46160d
Pelli <friedstudios@outlook.com>
2021-09-23 09:44:17 -0300

    Interface Created

--- ArithmeticCoding/Form1.Designer.cs
+++ ArithmeticCoding/Form1.Designer.cs
@@ -29,13 +29,107 @@ namespace ArithmeticCoding
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tittleLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.stringTextBox = new System.Windows.Forms.TextBox();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tittleLabel
+            // 
+            this.tittleLabel.AutoSize = true;
+            this.tittleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tittleLabel.Location = new System.Drawing.Point(173, 42);
+            this.tittleLabel.Name = "tittleLabel";
+            this.tittleLabel.Size = new System.Drawing.Size(272, 31);
+            this.tittleLabel.TabIndex = 0;
+            this.tittleLabel.Text = "Codificador Arimético";
+            this.tittleLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Location = new System.Drawing.Point(75, 136);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(144, 13);
+            this.inputLabel.TabIndex = 2;
+            this.inputLabel.Text = "Ingrese la cadena a codificar";
+            // 
+            // stringTextBox
+            // 
+            this.stringTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stringTextBox.Location = new System.Drawing.Point(78, 152);
+            this.stringTextBox.Name = "stringTextBox";
+            this.stringTextBox.Size = new System.Drawing.Size(446, 30);
+            this.stringTextBox.TabIndex = 3;
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Location = new System.Drawing.Point(247, 200);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(105, 32);
+            this.confirmButton.TabIndex = 4;
+            this.confirmButton.Text = "Codificar";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextBox.Location = new System.Drawing.Point(78, 254);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(446, 30);
+            this.outputTextBox.TabIndex = 6;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(75, 238);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(36, 13);
+            this.outputLabel.TabIndex = 5;
+            this.outputLabel.Text = "Salida";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(633, 361);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.confirmButton);
+            this.Controls.Add(this.stringTextBox);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tittleLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label tittleLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.TextBox stringTextBox;
+        private System.Windows.Forms.Button confirmButton;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Label outputLabel;
     }
 }
 
