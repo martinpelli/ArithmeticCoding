6aada7378e9a37ebb618339abdc03e6e0d019fd1
Pelli <friedstudios@outlook.com>
2021-09-23 15:59:03 -0300

    calculation of lower and upper bounds added

--- ArithmeticCoding/Form1.cs
+++ ArithmeticCoding/Form1.cs
@@ -22,8 +22,10 @@ namespace ArithmeticCoding
             Tuple<long[], char[]> lettersAndQuantity = calculateRepetitionForEachLetter();
             char[] lettersWithoutRepeat = lettersAndQuantity.Item2;
             long[] quantityForEachLetter = lettersAndQuantity.Item1;
-            double segmentOfMessage = calculateSegmentOfMessage(quantityForEachLetter);
-            MessageBox.Show(segmentOfMessage.ToString());
+            Tuple<double, double> lowerAndUpperBounds = calculateLowerAndUpperBounds(quantityForEachLetter,lettersWithoutRepeat);
+            double upperBound = lowerAndUpperBounds.Item2;
+            double lowerBound = lowerAndUpperBounds.Item1;
+
         }
 
         private Tuple<long[], char[]> calculateRepetitionForEachLetter()
@@ -63,15 +65,51 @@ namespace ArithmeticCoding
 
         }
 
-        private double calculateSegmentOfMessage(long[] quantityForEachLetter)
+        private Tuple<double, double> calculateLowerAndUpperBounds(long[] quantityForEachLetter,char[] lettersWithoutRepeat)
         {
-            double segmentSize = 0;
-            foreach (long quantity in quantityForEachLetter)
+            string message = stringTextBox.Text;
+            double messageSize = Convert.ToDouble(message.Length);
+            double upperBound = 1;
+            double lowerBound = 0;
+            double segmentOfMessage = calculateSegmentOfMessage(upperBound, messageSize);
+            for (int i = 0; i < message.Length; i++)
             {
-                segmentSize += Convert.ToDouble(quantity);
+
+                int positionOfLetter = 0;
+                long numberOfLettersBehind = 0;
+
+                for (int j = lettersWithoutRepeat.Length -1; j > -1; j--)
+                {
+                    if (message[i] == lettersWithoutRepeat[j])
+                    {
+                        //MessageBox.Show("Leyendo la letra: " + message[i].ToString());
+                        //MessageBox.Show("Tiene esta cant de letras atras: " + numberOfLettersBehind.ToString());
+                        upperBound = lowerBound + ((numberOfLettersBehind + quantityForEachLetter[j]) * segmentOfMessage);
+                        lowerBound += numberOfLettersBehind * segmentOfMessage;
+                        //MessageBox.Show("Cota Inferior: " + lowerBound.ToString());
+                        //MessageBox.Show("Cota Superior: " + upperBound.ToString());
+                        numberOfLettersBehind = 0;
+                        positionOfLetter = 0;
+                        break;
+                    }
+                    else
+                    {
+                        positionOfLetter += 1;
+                        numberOfLettersBehind += quantityForEachLetter[j];
+                    }
+                }
+                segmentOfMessage = calculateSegmentOfMessage(upperBound - lowerBound, messageSize);
+
             }
 
-            return 1 / segmentSize;
+            return Tuple.Create(lowerBound,upperBound);
         }
+
+        private double calculateSegmentOfMessage(double range, double messageSize)
+        {
+            return range / messageSize;
+        }
+
+
     }
 }
