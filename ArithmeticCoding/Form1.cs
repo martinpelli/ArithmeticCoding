33d604baf4add2aa1e1d2a6d664c06fbd5ffdbb8
Pelli <friedstudios@outlook.com>
2021-09-23 20:37:17 -0300

    Alphabetical order added

--- ArithmeticCoding/Form1.cs
+++ ArithmeticCoding/Form1.cs
@@ -19,16 +19,23 @@ namespace ArithmeticCoding
 
         private void confirmButton_Click(object sender, EventArgs e)
         {
-            Tuple<long[], char[]> lettersAndQuantity = calculateRepetitionForEachLetter();
-            char[] lettersWithoutRepeat = lettersAndQuantity.Item2;
-            long[] quantityForEachLetter = lettersAndQuantity.Item1;
-            Tuple<double, double> lowerAndUpperBounds = calculateLowerAndUpperBounds(quantityForEachLetter,lettersWithoutRepeat);
+            Tuple<List<long>, List<char>> lettersAndQuantityList = calculateRepetitionForEachLetter();
+            List<char> lettersWithoutRepeatList = lettersAndQuantityList.Item2;
+            List<long> quantityForEachLetterList = lettersAndQuantityList.Item1;
+
+            Tuple<long[], char[]> lettersAndQuantity = orderQuantityAndLetters(quantityForEachLetterList,lettersWithoutRepeatList);
+            
+            Tuple<long[], char[]> lettersAndQuantityOrdered = orderAlphabeticallySameQuantityLetters(lettersAndQuantity.Item1, lettersAndQuantity.Item2, 0);
+            char[] lettersWithoutRepeatArray = lettersAndQuantityOrdered.Item2;
+            long[] quantityForEachLetterArray = lettersAndQuantityOrdered.Item1;
+
+            Tuple<double, double> lowerAndUpperBounds = calculateLowerAndUpperBounds(quantityForEachLetterArray,lettersWithoutRepeatArray);
             double upperBound = lowerAndUpperBounds.Item2;
             double lowerBound = lowerAndUpperBounds.Item1;
 
         }
 
-        private Tuple<long[], char[]> calculateRepetitionForEachLetter()
+        private Tuple<List<long>, List<char>> calculateRepetitionForEachLetter()
         {
             string message = stringTextBox.Text;
             List<char> lettersWithoutRepeat = new List<char>();
@@ -55,14 +62,62 @@ namespace ArithmeticCoding
                     letterQuantity.Add(1);
                 }
             }
+
+            return Tuple.Create(letterQuantity, lettersWithoutRepeat);
+
+        }
+
+        private Tuple<long[], char[]> orderQuantityAndLetters(List<long> letterQuantity, List<char> lettersWithoutRepeat)
+        {
             long[] letterQuantityArray = letterQuantity.ToArray();
             char[] lettersWithoutRepeatArray = lettersWithoutRepeat.ToArray();
-            Array.Sort(letterQuantityArray,lettersWithoutRepeatArray);
+            Array.Sort(letterQuantityArray, lettersWithoutRepeatArray);
             Array.Reverse(letterQuantityArray);
             Array.Reverse(lettersWithoutRepeatArray);
 
             return Tuple.Create(letterQuantityArray, lettersWithoutRepeatArray);
+        }
 
+        private Tuple<long[], char[]> orderAlphabeticallySameQuantityLetters(long[] letterQuantity, char[] lettersWithoutRepeat,int from)
+        {
+            bool isFirst = false;
+            int to = 1;
+            int length = 1;
+
+            
+            for(int i = from; i < lettersWithoutRepeat.Length; i++)
+            {
+                if ((i+1 <lettersWithoutRepeat.Length) && letterQuantity[i] == letterQuantity[i + 1])
+                {
+                    if (!isFirst)
+                    {
+                        isFirst = true;
+                        from = i;
+                        to = from;
+                    }
+                    to += 1;
+                    length += 1;
+                    continue;
+                }
+                else
+                {
+                    if (isFirst)
+                    {
+                        break;
+                    }
+                }
+                
+            }
+            if (isFirst)
+            {
+                Array.Sort(lettersWithoutRepeat, from, length);
+                return orderAlphabeticallySameQuantityLetters(letterQuantity, lettersWithoutRepeat, to);
+            }
+            else
+            {
+                return Tuple.Create(letterQuantity, lettersWithoutRepeat);
+            }
+            
         }
 
         private Tuple<double, double> calculateLowerAndUpperBounds(long[] quantityForEachLetter,char[] lettersWithoutRepeat)
@@ -86,8 +141,8 @@ namespace ArithmeticCoding
                         //MessageBox.Show("Tiene esta cant de letras atras: " + numberOfLettersBehind.ToString());
                         upperBound = lowerBound + ((numberOfLettersBehind + quantityForEachLetter[j]) * segmentOfMessage);
                         lowerBound += numberOfLettersBehind * segmentOfMessage;
-                        //MessageBox.Show("Cota Inferior: " + lowerBound.ToString());
-                        //MessageBox.Show("Cota Superior: " + upperBound.ToString());
+                        MessageBox.Show("Cota Inferior: " + lowerBound.ToString());
+                        MessageBox.Show("Cota Superior: " + upperBound.ToString());
                         numberOfLettersBehind = 0;
                         positionOfLetter = 0;
                         break;
